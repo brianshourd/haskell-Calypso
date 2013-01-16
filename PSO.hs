d6705bdcad725e1a38b6fc3427ce2a56b76e4220
Brian Shourd <brian.shourd@gmail.com>
2013-01-16 13:45:57 -0500

    Renaming conventions - PSO -> Pso, PSOCand -> PsoGuide

--- PSO.hs
+++ PSO.hs
@@ -15,13 +15,13 @@ entry is also quite good:
 <http://en.wikipedia.org/wiki/Particle_swarm_optimization>
 -}
 
-module PSO 
+module Pso 
     (
-    PSOVect(..), 
-    PSOCand(..), 
+    PsoVect(..), 
+    PsoGuide(..), 
     Particle(..), 
     Swarm(..), 
-    PSOParams(..), 
+    PsoParams(..), 
     randomSwarm, 
     createSwarm, 
     updateSwarm, 
@@ -36,12 +36,12 @@ Represents the position and velocity of a particle.
 
 Minimal complete definition: @'pAdd'@, @'pZero'@, @'pScale'@
 
-Theoretically, a type belonging to the class @PSOVect@ should form a
+Theoretically, a type belonging to the class @PsoVect@ should form a
 /real vector space/
 (<http://en.wikipedia.org/wiki/Vector_space#Definition>). That is, it
 should satisfy the vector space laws: 
 
-Suppose that the type @a@ is an instance of @'PSOVect'@. for all @u@,
+Suppose that the type @a@ is an instance of @'PsoVect'@. for all @u@,
 @v@, and @w@ of type @a@, and all @r@ and @s@ of type @Double@, we
 should have:
 
@@ -75,7 +75,7 @@ Random is a requirement of this typeclass, as well. Especially randomR,
 which should work on a component-by-component basis. See the examples
 for more information on how/why Random is required.
 -}
-class (Random a) => PSOVect a where
+class (Random a) => PsoVect a where
     pAdd :: a -> a -> a
     pZero :: a
     pScale :: Double -> a -> a
@@ -83,13 +83,19 @@ class (Random a) => PSOVect a where
     pSubtract v1 v2 = pAdd v1 $ pScale (-1) v2
 
 {- |
-A candidate for a global minimum. Stores both the location of the
-possible minimum, and the value of the function to minimize at that
-point.
+A guide for a particle. The term "guide" originates from (as far as I
+can tell) this paper on velocity adaptation.
 
-In use, the type @a@ should belong to the @'PSOVect'@ class.
+Helwig, Sabine, Frank Neumann, and Rolf Wanka. "Particle swarm
+optimization with velocity adaptation." Adaptive and Intelligent
+Systems, 2009. ICAIS'09. International Conference on. IEEE, 2009.
+
+It stores both the location of the possible minimum, and the value of
+the function to minimize at that point.
+
+In use, the type @a@ should belong to the @'PsoVect'@ class.
 -}
-data PSOCand a = PSOCand {
+data PsoGuide a = PsoGuide {
     pt :: a, 
     val :: Double
     } deriving (Show)
@@ -99,12 +105,12 @@ data PSOCand a = PSOCand {
 they've visited. Individual @Particles@ do not know the global minimum,
 but the @Swarm@ they belong to does
 
-In use, the type @a@ should belong to the @'PSOVect'@ class.
+In use, the type @a@ should belong to the @'PsoVect'@ class.
 -}
 data Particle a = Particle {
     pos :: a,       -- ^ position of particle
     vel :: a,       -- ^ velocity of particle
-    pBest :: PSOCand a  -- ^ best position this particle has found
+    pGuide :: PsoGuide a  -- ^ private guide
     } deriving (Show)
 
 {- | 
@@ -114,11 +120,11 @@ Shi, Yuhui, and Russell Eberhart. \"Parameter selection in particle swarm
 optimization.\" /Evolutionary Programming VII./ Springer
 Berlin/Heidelberg, 1998.
 -}
-data PSOParams = PSOParamsStatic
+data PsoParams = PsoParamsStatic
     Double  -- inertia weight
     Double  -- tendancy toward local
     Double  -- tendancy toward global
-    | PSOParamsDynamic 
+    | PsoParamsDynamic 
     (Integer -> Double)  -- inertia weight
     (Integer -> Double)  -- tendancy toward local
     (Integer -> Double)  -- tendancy toward global
@@ -130,37 +136,37 @@ Optimization\" by James Kennedy and Russell Eberhart
 Normally, one should search for better parameters, since parameter
 choice dramatically influences algorithm performance.
 -}
-defaultPSOParams :: PSOParams
-defaultPSOParams = PSOParamsStatic 1 2 2
+defaultPsoParams :: PsoParams
+defaultPsoParams = PsoParamsStatic 1 2 2
 
 {- | 
 A @Swarm@ keeps track of all the particles in the swarm, the function
 that the swarm seeks to minimize, the parameters, the current iteration
 (for parameters), and the best location/value found so far
 
-In use, the type @a@ should belong to the @'PSOVect'@ class.
+In use, the type @a@ should belong to the @'PsoVect'@ class.
 -}
 data Swarm a = Swarm {
     parts :: [Particle a],  -- ^ particles in the swarm
-    gBest :: PSOCand a,     -- ^ best position found
+    gGuide :: PsoGuide a,   -- ^ global guide
     func :: a -> Double,    -- ^ funtion to minimize
-    params :: PSOParams,    -- ^ parameters
+    params :: PsoParams,    -- ^ parameters
     iteration :: Integer    -- ^ current iteration
     }
 
 instance Show a => Show (Swarm a) where
-    show (Swarm ps b _ _ _) = show ( map pBest ps) ++ show b
+    show (Swarm ps b _ _ _) = show ( map pGuide ps) ++ show b
 
 {- | 
 Create a swarm by randomly generating n points within the bounds, making
 all the particles start at these points with velocity zero.
 -}
-randomSwarm :: (PSOVect a, Random a, RandomGen b) 
+randomSwarm :: (PsoVect a, Random a, RandomGen b) 
     => b        -- ^ A random seed
     -> Int      -- ^ Number of particles
     -> (a,a)    -- ^ Bounds to create particles in
     -> (a -> Double)    -- ^ Function to minimize
-    -> PSOParams    -- ^ Parameters
+    -> PsoParams    -- ^ Parameters
     -> (Swarm a, b) -- ^ (Swarm returned, new seed)
 randomSwarm g n bounds f params = (createSwarm ps f params, g') where
     (ps, g') = getSomeRands n g []
@@ -172,15 +178,15 @@ randomSwarm g n bounds f params = (createSwarm ps f params, g') where
 Create a swarm in initial state based on the positions of the particles.
 Initial velocities are all zero.
 -}
-createSwarm :: (PSOVect a) 
+createSwarm :: (PsoVect a) 
     => [a]          -- ^ Positions of of particles
     -> (a -> Double)    -- ^ Function to minimize
-    -> PSOParams    -- ^ Parameters to use
+    -> PsoParams    -- ^ Parameters to use
     -> Swarm a
 createSwarm ps f pars = Swarm qs b f pars 0 where
     qs = map (createParticle f) ps
-    b = minimumBy (compare `on` val) $ map pBest qs
-    createParticle f' p = Particle p pZero (PSOCand p (f' p))
+    b = minimumBy (compare `on` val) $ map pGuide qs
+    createParticle f' p = Particle p pZero (PsoGuide p (f' p))
 
 {- | 
 Update the swarm one step, updating every particle's position and
@@ -189,26 +195,26 @@ well as a new generator to use.
 
 Arguments ordered to allow @iterate (uncurry updateSwarm)@
 -}
-updateSwarm :: (PSOVect a, RandomGen b) => Swarm a -> b -> (Swarm a, b)
+updateSwarm :: (PsoVect a, RandomGen b) => Swarm a -> b -> (Swarm a, b)
 updateSwarm s@(Swarm ps b f pars i) g = (Swarm qs b' f pars (i+1), g') where
     (qs, g', b') = foldl' helper ([], g, b) ps
     helper (acc, gen, best) p = (p':acc, gen', minBest) where
         (p',gen') = updateParticle p s gen
-        minBest = case compare (val best) (val $ pBest p') of
+        minBest = case compare (val best) (val $ pGuide p') of
             LT -> best
-            _  -> pBest p'
+            _  -> pGuide p'
 {- |
 Update a swarm repeatedly. Absorbs a @RandomGen@.
 -}
 
-iterateSwarm :: (PSOVect a, RandomGen b) => Swarm a -> b -> [Swarm a]
+iterateSwarm :: (PsoVect a, RandomGen b) => Swarm a -> b -> [Swarm a]
 iterateSwarm s g = map fst $ iterate (uncurry updateSwarm) (s,g)
 
 {- | 
 Update a particle one step. Called by updateSwarm and requires the swarm
 that the particle belongs to as a parameter
 -}
-updateParticle :: (PSOVect a, RandomGen b) => Particle a -> Swarm a -> b -> (Particle a, b)
+updateParticle :: (PsoVect a, RandomGen b) => Particle a -> Swarm a -> b -> (Particle a, b)
 updateParticle (Particle p v bp) (Swarm ps b f pars i) g = (Particle p' v' bp', g'') where
     p' = pAdd p v'
     dp = pSubtract (pt bp) p
@@ -218,14 +224,14 @@ updateParticle (Particle p v bp) (Swarm ps b f pars i) g = (Particle p' v' bp', 
     (r1, g') = randomR (pZero,dp) g
     (r2, g'') = randomR (pZero,dg) g'
     v' = newVel pars
-    newVel (PSOParamsStatic omega c1 c2) = pAdd (pScale omega v) $ 
+    newVel (PsoParamsStatic omega c1 c2) = pAdd (pScale omega v) $ 
         pAdd (pScale c1 dp) $
-        (pScale c2 dg)
-    newVel (PSOParamsDynamic omega c1 c2) = pAdd (pScale (omega i) v) $ 
+        pScale c2 dg
+    newVel (PsoParamsDynamic omega c1 c2) = pAdd (pScale (omega i) v) $ 
          pAdd (pScale (c1 i) dp) $
          pScale (c2 i) dg
     bp' = case compare (val bp) (f p') of
-        LT -> PSOCand p' (f p')
+        LT -> PsoGuide p' (f p')
         _  -> bp
 
 -- ===============
@@ -236,14 +242,14 @@ updateParticle (Particle p v bp) (Swarm ps b f pars i) g = (Particle p' v' bp', 
 Declarations for fractionals e.g Double, Rational
 -}
 
-instance PSOVect Double where
+instance PsoVect Double where
     pAdd = (+)
     pScale = (*)
     pZero = 0
     pSubtract = (-)
 
 {-
-instance PSOVect Rational where
+instance PsoVect Rational where
     pAdd = (+)
     pScale r = (*) (toRational r)
     pZero = 0
@@ -251,12 +257,12 @@ instance PSOVect Rational where
 -}
 
 {- 
-Declaration for pairs of PSOVects e.g. (Double, Double), (Float,
+Declaration for pairs of PsoVects e.g. (Double, Double), (Float,
 Float), (Rational, Rational), etc.
 
 Mathematically, this is the direct sum of vector spaces.
 -}
-instance (PSOVect a, PSOVect b, Random a, Random b) => Random (a, b) where
+instance (PsoVect a, PsoVect b, Random a, Random b) => Random (a, b) where
     random g = ((x, y), g'') where
         (x, g')  = random g
         (y, g'') = random g'
@@ -264,17 +270,17 @@ instance (PSOVect a, PSOVect b, Random a, Random b) => Random (a, b) where
         (x, g')  = randomR (a1, a2) g
         (y, g'') = randomR (b1, b2) g'
 
-instance (PSOVect a, PSOVect b) => PSOVect (a, b) where
+instance (PsoVect a, PsoVect b) => PsoVect (a, b) where
     pAdd (x1, y1) (x2, y2) = (pAdd x1 x2, pAdd y1 y2)
     pScale r (x,y) = (pScale r x, pScale r y)
     pZero = (pZero, pZero)
 
 {- 
-Declaration for triples of PSOVects
+Declaration for triples of PsoVects
 
 Mathematically, this is the direct sum of vector spaces.
 -}
-instance (PSOVect a, PSOVect b, PSOVect c, Random a, Random b, Random c) => Random (a, b, c) where
+instance (PsoVect a, PsoVect b, PsoVect c, Random a, Random b, Random c) => Random (a, b, c) where
     random g = ((x, y, z), g''') where
         (x, g')   = random g
         (y, g'')  = random g'
@@ -284,7 +290,7 @@ instance (PSOVect a, PSOVect b, PSOVect c, Random a, Random b, Random c) => Rand
         (y, g'')  = randomR (b1, b2) g'
         (z, g''') = randomR (c1, c2) g''
 
-instance (PSOVect a, PSOVect b, PSOVect c) => PSOVect (a, b, c) where
+instance (PsoVect a, PsoVect b, PsoVect c) => PsoVect (a, b, c) where
     pAdd (x1, y1, z1) (x2, y2, z2) = (pAdd x1 x2, pAdd y1 y2, pAdd z1 z2)
     pScale r (x,y,z) = (pScale r x, pScale r y, pScale r z)
     pZero = (pZero,pZero,pZero)
